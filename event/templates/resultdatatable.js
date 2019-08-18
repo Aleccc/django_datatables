3524c80bf2ba90121bbb401864658f2d11582f51
alec <alec.kaye@gmail.com>
2019-08-18 07:54:37 -0400

    add html and js for datatable

---
+++ event/templates/resultdatatable.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var dt_table = $('.datatable').dataTable({
+        "bFilter": true,
+        "iDisplayLength" : 25,
+        "bAutoWidth": true,
+        "bProcessing": true,
+        "bServerSide": true,
+        "bStateSave": true,
+        "sAjaxSource": DATA_TABLE_URL
+    });
+});
